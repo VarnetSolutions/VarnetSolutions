6833490cd83cbb9125d119cea395c10612a7f807
aniket <aniketshrivastava216@gmail.com>
2024-07-26 14:54:21 +0530

    add build

--- VarnetFileService/Properties/AssemblyInfo.cs
+++ VarnetFileService/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("VarnetFileService")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("My test nuget")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("test")]
 [assembly: AssemblyProduct("VarnetFileService")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyCopyright("Copyright © test 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
